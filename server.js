98b493e815cae87fa1f9c40dd00e4c64c6e9c5d4
jedelacarrera <jedelacarrera@uc.cl>
2022-01-16 01:40:05 -0800

    :wrench: serve routes

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 const port = process.env.PORT || 80
 app.listen(port)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
